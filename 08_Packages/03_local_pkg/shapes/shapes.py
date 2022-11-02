aac88ed135f5d93c2dd7dcab12a73ce88f062c24
Thrilok28021996 <altrathrill@gmail.com>
2022-11-02 19:22:07 +0530

    Packages

---
+++ 08_Packages/03_local_pkg/shapes/shapes.py
@@ -0,0 +1,12 @@
+# call from local packages
+from .rectangle import Rectangle
+from .circle import Circle
+from .square import Square
+
+c = Circle(5)
+r = Rectangle(23.12, 45.82)
+s = Square(7.80)
+
+print(c)
+print(r)
+print(s)
---
+++ 08_Packages/04_pkg_init/shapes/geometry.py
@@ -0,0 +1,12 @@
+from circle import Circle
+from rectangle import Rectangle
+from square import Square
+
+
+c = Circle(5)
+r = Rectangle(23.12, 45.82)
+s = Square(7.80)
+
+print(c)
+print(r)
+print(s)
